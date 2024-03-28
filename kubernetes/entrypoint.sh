8c8fce7332558fdab7f052fc383098374007168d
ku9nov <69673517+ku9nov@users.noreply.github.com>
2024-03-28 14:46:33 +0200

    Update entrypoint.sh

--- kubernetes/entrypoint.sh
+++ kubernetes/entrypoint.sh
@@ -14,7 +14,7 @@ bootstrap:
       use_pg_rewind: true
       pg_hba:
       - host all all 0.0.0.0/0 md5
-      - host replication ${PATRONI_REPLICATION_USERNAME} ${PATRONI_KUBERNETES_POD_IP}/16 md5
+      - host replication ${PATRONI_REPLICATION_USERNAME} ${PATRONI_KUBERNETES_POD_IP}-service/16 md5
   initdb:
   - auth-host: md5
   - auth-local: trust
@@ -22,9 +22,9 @@ bootstrap:
   - locale: en_US.UTF-8
   - data-checksums
 restapi:
-  connect_address: '${PATRONI_KUBERNETES_POD_IP}:8008'
+  connect_address: '${PATRONI_KUBERNETES_POD_IP}-service:8008'
 postgresql:
-  connect_address: '${PATRONI_KUBERNETES_POD_IP}:5432'
+  connect_address: '${PATRONI_KUBERNETES_POD_IP}-service:5432'
   authentication:
     superuser:
       password: '${PATRONI_SUPERUSER_PASSWORD}'
